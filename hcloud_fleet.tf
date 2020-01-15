ff4604533ff35c1e30c2b806d98f934579327eb9
Yannic Rieger <riegeryannic@gmail.com>
2020-01-15 14:10:03 +0100

    Add new servers for kubernetes cluster

--- hcloud_fleet.tf
+++ hcloud_fleet.tf
@@ -2,28 +2,22 @@ data "hcloud_ssh_key" "ssh_key" {
   fingerprint = "${var.public_key}"
 }
 
-resource "hcloud_server" "hub" {
-  name        = "bungee"
+resource "hcloud_server" "controlplane" {
+  name        = "controlplane"
   image       = "debian-9"
   server_type = "cx11"
-  ssh_keys = ["${data.hcloud_ssh_key.ssh_key.id}"]
-  connection {
-    type     = "ssh"
-    user     = "root"
-    private_key = file("${var.private_key}")
-    host     = "${self.ipv4_address}"
-  }
-  provisioner "file" {
-    content     = templatefile("config/bungee_config.yml.template", { 
-      lobby_address = "${self.ipv4_address}:25566"
-      flash01_address = "${hcloud_server.flash01.ipv4_address}" 
-    })
-    destination = "/etc/config.yml"
-  }
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
 }
 
-resource "hcloud_server" "flash01" {
-  name        = "flash01"
+resource "hcloud_server" "etcd" {
+  name        = "etcd"
+  image       = "debian-9"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+}
+
+resource "hcloud_server" "worker" {
+  name        = "worker"
   image       = "debian-9"
   server_type = "cx11"
   ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
