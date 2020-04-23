f1210644bbd1082cb0608d8588617fbfa2395f8f
Yannic Rieger <yannic.rieger@hetzner-cloud.de>
2020-04-23 17:17:54 +0200

    Restructure layout

---
+++ terraform/prod/k8s_cluster.tf
@@ -0,0 +1,45 @@
+data "hcloud_ssh_key" "ssh_key" {
+  fingerprint = "${var.public_key}"
+}
+
+resource "hcloud_server" "prod-k8s-etcd01" {
+  name        = "prod-k8s-etcd01"
+  image       = "debian-9"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    type = "etcd"
+  }
+}
+
+resource "hcloud_server" "prod-k8s-master01" {
+  name        = "prod-k8s-master01"
+  image       = "debian-9"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    type = "master"
+  }
+}
+
+resource "hcloud_server" "prod-k8s-worker01" {
+  name        = "prod-k8s-worker01"
+  image       = "debian-9"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    type = "worker"
+    workload_type = "spigot"
+  }
+}
+
+resource "hcloud_server" "prod-k8s-worker02" {
+  name        = "prod-k8s-worker02"
+  image       = "debian-9"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    type = "worker"
+    workload_type = "bungee"
+  }
+}
